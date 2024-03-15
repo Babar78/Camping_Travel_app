227bb7afcac90de93ed522be491bb9314eb7c249
Babar78 <muhammadbabarashraf8@gmail.com>
2024-03-15 21:07:49 +0500

    Home Page's Section Components Intialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Camp from "@/components/Home/Camp";
+import Features from "@/components/Home/Features";
+import GetApp from "@/components/Home/GetApp";
+import Guide from "@/components/Home/Guide";
+import Hero from "@/components/Home/Hero";
+
 export default function Home() {
-  return <h1 className="font-bold text-teal-400 text-2xl">Hello World</h1>;
+  return (
+    <> 
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
